5ac2f3546668131b0d5dd04937daf2160e346ded
Mulekotd <joaopedro.gama0675@gmail.com>
2026-06-08 22:14:51 -0300

    feat: Add nextjs frontend and improve docker-compose for development environment

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# Arduino Contest
+# Klima
 
+## Overview
 
+Klima is an IoT solution developed for the IEEE SSCS Arduino Contest. It transforms conventional air conditioners into smart, connected devices through a combination of microcontrollers and environmental sensors, including temperature, humidity, and motion detection. The system is complemented by a Progressive Web Application (PWA) that enables remote control, automated climate management, energy consumption monitoring, and cost analysis, providing users with greater comfort, efficiency, and sustainability.
+
+---
+
+## Installation and Setup
+
+### 1. Clone the Repository
+
+```bash
+git clone https://github.com/Mulekotd/klima-iot.git
+cd klima-iot
+```
+
+## Contributors
+
+<a href="https://github.com/Mulekotd/klima-iot/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=Mulekotd/klima-iot" />
+</a>
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Klima",
+  description: "Klima is a smart climate management solution that upgrades any air conditioner with automation, remote control, and energy monitoring. Designed to improve comfort while reducing electricity costs, Klima brings the convenience of a modern smart home to existing cooling systems—without the expense of buying a new smart air conditioner.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
